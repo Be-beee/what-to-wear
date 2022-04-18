b6a3ae6937d429cf21f78fbb4e0a8876afbb96e2
bokyungseo <maybutter756@gmail.com>
2022-04-18 21:08:27 +0900

    Create project

---
+++ whatToWear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  whatToWear
+//
+//  Created by 서보경 on 2022/04/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ whatToWear/whatToWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatToWearApp.swift
+//  whatToWear
+//
+//  Created by 서보경 on 2022/04/18.
+//
+
+import SwiftUI
+
+@main
+struct whatToWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
